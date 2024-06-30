4067aa794e99b2ecc2ec6657b5c74422d67019d3
Akash Saha <akash5@protonmail.com>
2024-06-30 14:15:03 +0530

    added some basic tests

--- src/test/java/com/example/demo/HelloWorldApplicationTests.java
+++ src/test/java/com/example/demo/HelloWorldApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HelloWorldApplicationTests {
+	
+	@LocalServerPort
+	private int port;
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Autowired
+	private MainController mainController;
+	
 	@Test
 	void contextLoads() {
+		assertThat(mainController).isNotNull();
 	}
-
+	
+	@Test
+	void testHello() {
+		String url = String.format("http://localhost:%d/", port);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+		assertThat(response.getBody()).contains("hello, world");
+	}
 }
